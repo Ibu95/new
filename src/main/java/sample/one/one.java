6314e957b1173f4fcf206fac4c70778bb50c4ed1
ibu95 <ibu95.ia@gmail.com>
2021-09-27 15:16:23 +0530

    my folder

---
+++ src/main/java/sample/one/one.java
@@ -0,0 +1,12 @@
+package sample.one;
+
+public class one {
+	
+		public static void two() {
+	System.out.println("employee");
+		}
+		public static void main(String[] args) {
+			one.two();
+		}
+}
+
